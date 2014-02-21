43b0f5b036c17216e29ef4fa8741e9cf3ffa1893
Mike Boers <github@mikeboers.com>
2014-02-21 15:48:23 -0500

    Start of project

---
+++ fbo_sandbox.py
@@ -0,0 +1,138 @@
+from __future__ import division
+
+import sys
+import time
+import math
+import random
+import os
+
+from mygl import gl, glu, glut
+
+
+class App(object):
+    
+    def __init__(self, argv):
+    
+        # Initialize GLUT and out render buffer.
+        glut.init(argv)
+        glut.initDisplayMode(glut.DOUBLE | glut.RGBA | glut.DEPTH | glut.MULTISAMPLE)
+    
+        # Initialize the window.
+        self.width = 800
+        self.height = 600
+        glut.initWindowSize(self.width, self.height)
+        glut.createWindow(argv[0])
+
+        gl.clearColor(0, 0, 0, 1)
+    
+        # Turn on a bunch of OpenGL options.
+        gl.enable(gl.CULL_FACE)
+        gl.enable(gl.DEPTH_TEST)
+        gl.enable(gl.COLOR_MATERIAL)
+        gl.enable('blend')
+        gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+        gl.enable('multisample')
+        
+
+        self.fbo = gl.genFramebuffers(1)
+        gl.bindFramebuffer(gl.FRAMEBUFFER, self.fbo)
+
+        self.render_texture = gl.genTextures(1)
+        gl.bindTexture(gl.TEXTURE_2D, self.render_texture)
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, self.width, self.height, 0, gl.RGB, gl.UNSIGNED_BYTE, 0)
+
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, self.render_texture, 0)
+
+
+        gl.bindFramebuffer(gl.FRAMEBUFFER, 0)
+
+
+        # Attach some GLUT event callbacks.
+        glut.reshapeFunc(self.reshape)
+        glut.displayFunc(self.display)
+        glut.keyboardFunc(self.keyboard)
+        
+        self.frame_rate = 24.0
+        glut.timerFunc(int(1000 / self.frame_rate), self.timer, 0)
+        
+    
+    def keyboard(self, key, mx, my):
+        if key == '\x1b': # ESC
+            exit(0)
+        elif key == 'f':
+            glut.fullScreen()
+        else:
+            print 'unknown key %r at %s,%d' % (key, mx, my)
+            
+    def run(self):
+        return glut.mainLoop()
+
+    def reshape(self, width, height):
+        """Called when the user reshapes the window."""
+        self.width = width
+        self.height = height
+        
+        gl.viewport(0, 0, width, height)
+        gl.matrixMode(gl.PROJECTION)
+        gl.loadIdentity()
+        gl.ortho(0, width, 0, height, -100, 100)
+        gl.matrixMode(gl.MODELVIEW)
+        
+    def timer(self, value):
+        glut.postRedisplay()
+        glut.timerFunc(int(1000 / self.frame_rate), self.timer, 0)
+
+    def display(self):
+    
+        gl.bindFramebuffer(gl.FRAMEBUFFER, self.fbo)
+
+        # Wipe the window.
+        gl.disable('depth_test')
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+        gl.loadIdentity()
+        
+        gl.disable('texture_2d')
+
+        gl.color(1, 1, 0, 1)
+        with gl.begin('polygon'):
+            gl.vertex(0, 0)
+            gl.vertex(400, 0)
+            gl.vertex(400, 400)
+            gl.vertex(0, 400)
+
+        with gl.matrix():
+            with gl.begin('line_strip'):
+                for i in xrange(50):
+                    gl.vertex(self.width * random.random(), self.height * random.random(), 0)
+
+        
+
+        gl.bindFramebuffer(gl.FRAMEBUFFER, 0)
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+        gl.enable('texture_2d')
+        # gl.enable('depth_test')
+        gl.color(1, 1, 1, 1)
+        with gl.begin('polygon'):
+            gl.texCoord(0, 0)
+            gl.vertex(0, 0)
+            gl.texCoord(1, 0)
+            gl.vertex(200, 0)
+            gl.texCoord(1, 1)
+            gl.vertex(200, 200)
+            gl.texCoord(0, 1)
+            gl.vertex(0, 200)
+
+
+
+        glut.swapBuffers()
+
+        
+if __name__ == '__main__':
+    app = App(sys.argv)
+    exit(app.run())
+
+    
---
+++ mygl.py
@@ -0,0 +1,113 @@
+'''Mikes wrapper for the visualizer???'''
+from contextlib import contextmanager
+
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from OpenGL.GL.framebufferobjects import *
+from OpenGL.GL import *
+import OpenGL
+
+
+__all__ = '''
+    gl
+    glu
+    glut
+'''.strip().split()
+
+
+class ModuleProxy(object):
+    
+    def __init__(self, name, *modules):
+        self._name = name
+        self._modules = modules
+        self._module = modules[0]
+    
+    def __getattr__(self, raw_name):
+
+        # Constants.
+        if raw_name.isupper():
+            name = self._name.upper() + '_' + raw_name
+
+        # Methods.
+        else:
+            name = raw_name.split('_')
+            name = [x[0].upper() + x[1:] for x in name]
+            name = self._name + ''.join(name)
+
+        for module in self._modules:
+            try:
+                value = getattr(module, name)
+            except AttributeError:
+                continue
+            else:
+                setattr(self, raw_name, value)
+                setattr(self, name, value)
+                return value
+
+        raise AttributeError(raw_name)
+
+
+class GLProxy(ModuleProxy):
+    
+    @contextmanager
+    def matrix(self):
+        self._module.glPushMatrix()
+        try:
+            yield
+        finally:
+            self._module.glPopMatrix()
+    
+    @contextmanager
+    def attrib(self, *args):
+        mask = 0
+        for arg in args:
+            if isinstance(arg, basestring):
+                arg = getattr(self._module, 'GL_%s_BIT' % arg.upper())
+            mask |= arg
+        self._module.glPushAttrib(mask)
+        try:
+            yield
+        finally:
+            self._module.glPopAttrib()
+    
+    def enable(self, *args, **kwargs):
+        self._enable(True, args, kwargs)
+        return self._apply_on_exit(self._enable, False, args, kwargs)
+    
+    def disable(self, *args, **kwargs):
+        self._enable(False, args, kwargs)
+        return self._apply_on_exit(self._enable, True, args, kwargs)
+    
+    def _enable(self, enable, args, kwargs):
+        todo = []
+        for arg in args:
+            if isinstance(arg, basestring):
+                arg = getattr(self._module, 'GL_%s' % arg.upper())
+            todo.append((arg, enable))
+        for key, value in kwargs.iteritems():
+            flag = getattr(self._module, 'GL_%s' % key.upper())
+            value = value if enable else not value
+            todo.append((flag, value))
+        for flag, value in todo:
+            if value:
+                self._module.glEnable(flag)
+            else:
+                self._module.glDisable(flag)
+        
+    def begin(self, arg):
+        if isinstance(arg, basestring):
+            arg = getattr(self._module, 'GL_%s' % arg.upper())
+        self._module.glBegin(arg)
+        return self._apply_on_exit(self._module.glEnd)
+    
+    @contextmanager
+    def _apply_on_exit(self, func, *args, **kwargs):
+        try:
+            yield
+        finally:
+            func(*args, **kwargs)
+        
+
+gl = GLProxy('gl', OpenGL.GL, OpenGL.GL.framebufferobjects)
+glu = ModuleProxy('glu', OpenGL.GLU)
+glut = ModuleProxy('glut', OpenGL.GLUT)
