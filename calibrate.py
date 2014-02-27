0cdd4b7ee9d84382f17512fd2470a38a2c4c58e4
Mike Boers <github@mikeboers.com>
2014-02-26 19:50:50 -0500

    calibration works

--- calibrate.py
+++ calibrate.py
@@ -14,55 +14,65 @@ import numpy as np
 parser = argparse.ArgumentParser()
 parser.add_argument('-x', '--width', type=int, default=18)
 parser.add_argument('-y', '--height', type=int, default=9)
-parser.add_argument('-g', '--grids')
+parser.add_argument('-s', '--size', type=float, default=1.0)
+parser.add_argument('-t', '--temp')
 parser.add_argument('image', nargs='+')
 
 args = parser.parse_args()
 
 pattern_size = (args.width, args.height)
 
-base_object_points = []
-for x in xrange(args.width):
-    for y in xrange(args.height):
-        base_object_points.append((x, y))
+pattern_points = np.zeros((np.prod(pattern_size), 3), np.float32 )
+pattern_points[:,:2] = np.indices(pattern_size).T.reshape(-1, 2)
+pattern_points *= args.size
 
 
-image_points = []
+obj_points = []
+img_points = []
+
 
 for image_i, path in enumerate(args.image):
 
     print path
     image = cv2.imread(path)
     w, h = image.shape[:2]
-    image = cv2.resize(image, (2048, int(2048 * w / h)))
+    image = cv2.resize(image, (1024, int(1024 * w / h)))
 
-    if args.grids:
-        new_path = os.path.join(args.grids, os.path.basename(path))
-        cv2.imwrite(new_path, image)
-
-    print 'finding rough corners'
+    print '  finding rough corners'
     found, corners = cv2.findChessboardCorners(image, pattern_size, 0, cv.CV_CALIB_CB_ADAPTIVE_THRESH)
-    # print found, corners
+    if found:
+        print '  found', corners.shape
 
-    # print 'finding fine corners'
-    # TODO: move this back to the large frame.
-    # cv2.cornerSubPix(image, corners, (5, 5))
+        # print 'finding fine corners'
+        # TODO: move this back to the large frame.
+        print '  refine corners'
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        cv2.cornerSubPix(image[...,1], corners, (5, 5), (-1,-1), criteria)
 
-    cv2.drawChessboardCorners(image, pattern_size, corners, found)
+        cv2.drawChessboardCorners(image, pattern_size, corners, found)
 
-    if args.grids:
-        new_path = os.path.join(args.grids, os.path.basename(path) + '.grid.jpg')
+        img_points.append(corners.reshape(-1, 2))
+        obj_points.append(pattern_points)
+
+    if args.temp:
+        new_path = os.path.join(args.temp, os.path.basename(path))
         cv2.imwrite(new_path, image)
 
+print '---'
 
-    continue
-    print 'calibrating'
-    object_points = np.array([base_object_points for _ in xrange(image_i + 1)])
-    image_points.append(corners)
-    res = cv2.calibrateCamera(object_points, np.array(image_points), image.shape[:2])
-    print res
-    print
+rms, camera_matrix, dist_coefs, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, image.shape[:2], None, None)
+print "RMS:", rms
+print "camera matrix:\n", camera_matrix
+print "distortion coefficients: ", dist_coefs.ravel()
+print "rvecs: ", rvecs
+print "tvecs: ", tvecs
 
+print '---'
 
-
+fovx, fovy, focal_length, principal_point, aspect_ratio = cv2.calibrationMatrixValues(camera_matrix, image.shape[:2], 23.9, 35.8)
+print 'fovx:', fovx
+print 'fovy:', fovy
+print 'focal_length:', focal_length
+print 'principal_point:', principal_point
+print 'aspect_ratio:', aspect_ratio
 
